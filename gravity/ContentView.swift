8bc42acb536cc49c0d8295c0b47ecb1e92311399
akimasa-l <liumingzheng8@outlook.com>
2024-04-13 12:20:38 +0900

    すごいね〜

--- gravity/ContentView.swift
+++ gravity/ContentView.swift
@@ -8,13 +8,68 @@
 import SwiftUI
 import SwiftData
 
+struct MyGridItem:View {
+    var body: some View {
+        RoundedRectangle(cornerSize: .init(width: 3, height: 3))
+            .frame(width: 30, height: 30)
+            .foregroundColor(.blue)
+    }
+}
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
     var body: some View {
-        VStack{
-            Text("なんとかしたい")
+        Grid(horizontalSpacing: 5, verticalSpacing: 5) {
+            GridRow {
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+            }
+            GridRow {
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+            }
+            GridRow {
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+            }
+            GridRow {
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+            }
+            GridRow {
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+            }
+            GridRow {
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+                MyGridItem()
+            }
         }
     }
 }
