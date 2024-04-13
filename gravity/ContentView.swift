9cdb60513718b41b8f41db3b7c985bd71bdd291e
akimasa-l <liumingzheng8@outlook.com>
2024-04-13 16:37:00 +0900

    どうやってTimer使うのかよくわかんなねえ

--- gravity/ContentView.swift
+++ gravity/ContentView.swift
@@ -18,6 +18,11 @@ struct MyGridItemData:Hashable{
     var index_i:Int
     var index_j:Int
     var ball: Ball?
+    init(index_i: Int, index_j: Int, ball: Ball? = nil) {
+        self.index_i = index_i
+        self.index_j = index_j
+        self.ball = ball
+    }
 }
 
 struct MyGridItemView:View {
@@ -36,23 +41,38 @@ struct MyGridItemView:View {
 }
 
 struct ContentView: View {
-    var myGridItemDataList: [[MyGridItemData]] = (1..<10).map{
+      var myGridItemDataList: [[MyGridItemData]] = (1..<10).map{
         i in
         (1..<10).map{
             j in
-            if Float.random(in: 0...1)<0.2{
-                return MyGridItemData(index_i: i, index_j: j,ball: Ball())
+            if Float.random(in: 0...1) < 0.2 {
+                return MyGridItemData(index_i: i, index_j: j, ball: Ball())
             }else{
                 return MyGridItemData(index_i: i, index_j: j)
             }
         }
     }
+//    @State var timer:Timer
     var body: some View {
         Grid(horizontalSpacing: 5, verticalSpacing: 5) {
-            ForEach(myGridItemDataList,id:\.self){i in
+            ForEach(myGridItemDataList,id:\.self){ i in
                 GridRow {
-                    ForEach(i,id: \.self){j in
+                    ForEach(i,id: \.self){ j in
                         MyGridItemView(myGridItemData:j)
+                    }
+                }
+            }
+        }
+    }
+    func update(){
+        for i in myGridItemDataList{
+            for j in i{
+                if j.ball != nil{
+                    if j.index_j+1 >= myGridItemDataList[j.index_i].count{
+                        continue
+                    }
+                    if myGridItemDataList[j.index_i][j.index_j+1].ball == nil{
+//                        myGridItemDataList[j.index_i][j.index_j+1].ball = myGridItemDataList[j.index_i][j.index_j].ball
                     }
                 }
             }
