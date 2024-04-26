2f3d34ed807a5d431ac1e897af94cb580291b7b9
akimasa-l <liumingzheng8@outlook.com>
2024-04-26 10:45:30 +0900

    ちょっと良くないけどまあおk

--- gravity/ContentView.swift
+++ gravity/ContentView.swift
@@ -6,12 +6,11 @@
 //
 
 import SwiftUI
-//import SwiftData
-//import CoreMotion
+import CoreMotion
 
 struct Ball:Hashable{
-    var v_x:Float=0
-    var v_y:Float=0
+    var v_x:Double=0
+    var v_y:Double=0
 }
 
 struct MyGridItemData:Hashable{
@@ -45,6 +44,12 @@ let j_count=20
 
 let probability = 0.1
 
+let mass = 1.0
+let threshold = 0.1
+let friction = 0.03
+let bounce = 0.6
+let interval = 0.1
+
 struct ContentView: View {
     @State var myGridItemDataList: [[MyGridItemData]] = (0..<i_count).map{
         i in
@@ -52,12 +57,14 @@ struct ContentView: View {
             j in
             if Double.random(in: 0...1) < probability {
                 return MyGridItemData(index_i: i, index_j: j, ball: Ball())
-            }else{
+            } else {
                 return MyGridItemData(index_i: i, index_j: j)
             }
         }
     }
-    @State var timer:Timer?
+    @State var motionManager = CMMotionManager()
+    @State var a_x = 0.0
+    @State var a_y = 0.0
     var body: some View {
         Grid(horizontalSpacing: 5, verticalSpacing: 5) {
             ForEach(myGridItemDataList,id:\.self){ i in
@@ -68,23 +75,76 @@ struct ContentView: View {
                 }
             }
         }.onAppear{
-            timer=Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true){
-                _ in
-                update()
+            if motionManager.isDeviceMotionAvailable {
+                motionManager.accelerometerUpdateInterval = interval
+                motionManager.startAccelerometerUpdates(to: .main){
+                    (data,error) in
+                    guard let data else{return}
+                    self.a_x = data.acceleration.x //xが増える方向にjが増える
+                    self.a_y = -data.acceleration.y //yが減る方向にiが増える
+                    update()
+                }
             }
         }
     }
     func update(){
         for i in myGridItemDataList{
             for j in i{
-                if j.ball != nil{
+                guard var ball = j.ball else{continue}
+                ball.v_x += a_x * interval
+                ball.v_y += a_y * interval
+                if ball.v_x*mass > threshold {
                     if j.index_j+1 >= j_count{
+                        ball.v_x = ball.v_x * (-0.8)
+                        myGridItemDataList[j.index_i][j.index_j].ball = ball
                         continue
                     }
                     if myGridItemDataList[j.index_i][j.index_j+1].ball == nil{
-                        myGridItemDataList[j.index_i][j.index_j+1].ball = myGridItemDataList[j.index_i][j.index_j].ball
+                        ball.v_x -= friction/mass
+                        myGridItemDataList[j.index_i][j.index_j+1].ball = ball
                         myGridItemDataList[j.index_i][j.index_j].ball = nil
                     }
+                } else if ball.v_x*mass < (-threshold){
+                    if j.index_j-1 < 0{
+                        ball.v_x = ball.v_x * (-0.8)
+                        myGridItemDataList[j.index_i][j.index_j].ball = ball
+                        continue
+                    }
+                    if myGridItemDataList[j.index_i][j.index_j-1].ball == nil{
+                        ball.v_x += friction/mass
+                        myGridItemDataList[j.index_i][j.index_j-1].ball = ball
+                        myGridItemDataList[j.index_i][j.index_j].ball = nil
+                    }
+                } else if ball.v_y*mass > threshold{
+                    if j.index_i+1 >= i_count{
+                        ball.v_y = ball.v_y * (-0.8)
+                        myGridItemDataList[j.index_i][j.index_j].ball = ball
+                        continue
+                    }
+                    if myGridItemDataList[j.index_i+1][j.index_j].ball == nil{
+                        ball.v_y -= friction/mass
+                        myGridItemDataList[j.index_i+1][j.index_j].ball = ball
+                        myGridItemDataList[j.index_i][j.index_j].ball = nil
+                    }
+                } else if ball.v_y*mass < (-threshold){
+                    if j.index_i-1 < 0{
+                        ball.v_y = ball.v_y * (-0.8)
+                        myGridItemDataList[j.index_i][j.index_j].ball = ball
+                        continue
+                    }
+                    if myGridItemDataList[j.index_i-1][j.index_j].ball == nil{
+                        ball.v_y += friction/mass
+                        myGridItemDataList[j.index_i-1][j.index_j].ball = ball
+                        myGridItemDataList[j.index_i][j.index_j].ball = nil
+                    }
+                } else {
+                    if j.index_i-1 < 0 || j.index_i+1 >= i_count{
+                        ball.v_y=0.0
+                    }
+                    if j.index_j-1 < 0 || j.index_j+1 >= j_count{
+                        ball.v_x=0.0
+                    }
+                    myGridItemDataList[j.index_i][j.index_j].ball = ball
                 }
             }
         }
